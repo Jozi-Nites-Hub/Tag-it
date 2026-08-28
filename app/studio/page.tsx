0d921ada78743bfdaa42dc0f3d85771a280d2a7b
John Black Pipes <johnblackpipes@gmail.com>
2026-08-28 20:03:08 +0200

    Create page.tsx

---
+++ app/studio/page.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/Navbar";
+import Studio from "@/components/Studio";
+
+export default function StudioPage() {
+  return (
+    <main className="relative min-h-screen">
+      {/* Fixed Joburg Wallpaper */}
+      <div className="fixed inset-0 -z-10">
+        <div
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: "url('/joburg.jpg')" }}
+        />
+        <div className="absolute inset-0 bg-black/60" />
+        <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-transparent to-black/80" />
+      </div>
+
+      <Navbar />
+      <Studio />
+    </main>
+  );
+}
