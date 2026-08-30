a2f348499c636033e3b923a0beecb75497ea32c4
John Black Pipes <johnblackpipes@gmail.com>
2026-08-31 00:56:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,31 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  transpilePackages: ["@imgly/background-removal"],
   images: {
     unoptimized: true,
   },
-  // Needed for the WASM model
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       "sharp$": false,
-      "onnxruntime-node$": false,
+      "onnxruntime-node": false,
     };
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
     return config;
   },
 };
